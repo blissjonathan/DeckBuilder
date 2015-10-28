3c50428c3194d2ffd8d1a605dfe4e0a77d3cc273
Jonathan Bliss <blissjonathan1@gmail.com>
2015-10-28 13:24:40 -0400

    Panels

--- DeckBuilder/src/MainWindow.java
+++ DeckBuilder/src/MainWindow.java
@@ -4,6 +4,9 @@ import javax.swing.JFrame;
 import java.awt.Color;
 import java.awt.CardLayout;
 import javax.swing.JPanel;
+import javax.swing.JMenuBar;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
 
 public class MainWindow {
 
@@ -40,12 +43,22 @@ public class MainWindow {
 		frmDeckbuilder.setBackground(Color.LIGHT_GRAY);
 		frmDeckbuilder.setResizable(false);
 		frmDeckbuilder.setTitle("DeckBuilder .1");
-		frmDeckbuilder.setBounds(100, 100, 450, 300);
+		frmDeckbuilder.setBounds(100, 100, 450, 600);
 		frmDeckbuilder.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmDeckbuilder.getContentPane().setLayout(new CardLayout(0, 0));
 		
 		JPanel panel = new JPanel();
 		frmDeckbuilder.getContentPane().add(panel, "name_749988116922344");
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1, BorderLayout.NORTH);
+		
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2, BorderLayout.WEST);
+		
+		JPanel panel_3 = new JPanel();
+		panel.add(panel_3, BorderLayout.CENTER);
 	}
 
 }
