6251935aba99187d1f52cc77a6e3e93e7ab679ea
Jonathan Bliss <blissjonathan1@gmail.com>
2015-10-28 13:20:09 -0400

    WindowUpdate

--- DeckBuilder/src/MainWindow.java
+++ DeckBuilder/src/MainWindow.java
@@ -1,10 +1,13 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.CardLayout;
+import javax.swing.JPanel;
 
 public class MainWindow {
 
-	private JFrame frame;
+	private JFrame frmDeckbuilder;
 
 	/**
 	 * Launch the application.
@@ -14,7 +17,7 @@ public class MainWindow {
 			public void run() {
 				try {
 					MainWindow window = new MainWindow();
-					window.frame.setVisible(true);
+					window.frmDeckbuilder.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -33,9 +36,16 @@ public class MainWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmDeckbuilder = new JFrame();
+		frmDeckbuilder.setBackground(Color.LIGHT_GRAY);
+		frmDeckbuilder.setResizable(false);
+		frmDeckbuilder.setTitle("DeckBuilder .1");
+		frmDeckbuilder.setBounds(100, 100, 450, 300);
+		frmDeckbuilder.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmDeckbuilder.getContentPane().setLayout(new CardLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		frmDeckbuilder.getContentPane().add(panel, "name_749988116922344");
 	}
 
 }
