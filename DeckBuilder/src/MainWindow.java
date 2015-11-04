ce045047be409bc97c17ae0693536d2c2f20a5b8
Jonathan Bliss <blissjonathan1@gmail.com>
2015-11-04 13:53:14 -0500

    Card images

--- DeckBuilder/src/MainWindow.java
+++ DeckBuilder/src/MainWindow.java
@@ -79,14 +79,14 @@ Icon druidIcon = new ImageIcon("./resources/icons/druidicon.png");
 	 */
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 		
-		try {
-			response = Unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards")
-					.header("X-Mashape-Key", "32dPU6CVE4mshjYVlOQh1au6LxYVp1hAEkIjsnw7zXqucLcZY3")
-					.asJson();
-		} catch (UnirestException e1) {
+		//try {
+			//response = Unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards")
+				//	.header("X-Mashape-Key", "32dPU6CVE4mshjYVlOQh1au6LxYVp1hAEkIjsnw7zXqucLcZY3")
+			//		.asJson();
+		//} catch (UnirestException e1) {
 			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+			//e1.printStackTrace();
+		//}
 
 		
 		File[] directoryListing = dir.listFiles();
